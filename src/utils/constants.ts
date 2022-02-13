b0d2e4ba1f02363b3ef0e35e1a6dc6ab4bb6d41f
unsteelix <unsteelix@yandex.ru>
2022-02-13 14:13:54 +0400

    some env fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv'
-const env = dotenv.config()
+const env: any = dotenv.config()
 
 const PORT = 7400
 const filesDB_filepath = './volume/db.json'
-const PASS = env.PASS || '12345678'
-const TOKEN = env.TOKEN || 'fpjiaweflawlejofiwefho'
+const PASS = 'PASS' in env ? env.PASS : '12345678'
+const TOKEN = 'TOKEN' in env ? env.TOKEN : 'fpjiaweflawlejofiwefho'
 
 export default {
 	PORT,
 	filesDB_filepath,
 	PASS,
-	TOKEN
+	TOKEN,
 }
